d08c1239a13504e208ba05492132e994cb024243
Opilane TTHK <opilane@tthk.ee>
2020-09-08 14:14:05 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace StringInter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string FirstName;
+            string LastName;
+            Console.WriteLine("what is your name?");
+            FirstName = Console.ReadLine();
+
+            Console.WriteLine("what is your last name?");
+            LastName = Console.ReadLine();
+            // Console.WriteLine("hello," + FirstName + " " + LastName);
+
+            Console.WriteLine($"hello, {FirstName} {LastName} ");
+
+        }
+    }
+}
